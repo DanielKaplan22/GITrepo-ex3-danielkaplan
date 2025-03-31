57ce0658b6b47cc59a9d26a02622e5984316a48d
Daniel Kaplan <danielkaplan168@gmail.com>
2025-03-31 04:21:20 +0300

    the file DanielKaplan.java was updated

--- DanielKaplan.java
+++ DanielKaplan.java
@@ -2,7 +2,7 @@ public class DanielKaplan
 {
 	public static void main(String[] args){
 		for(int i = 0; i<5; i++){
-			System.out.println("My name is Daniel Kaplan");
+			System.out.println("not recognized");
 		}
 	}
 }
